ad739a339b9c21630d261ce81eb0e9fe65018741
gunitptvz <ptvz@inbox.ru>
2017-05-11 21:42:15 +0300

    Last commit from 11/05/2017

---
+++ Test Project Demo/SampleTest.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_Project_Demo
+{
+    [TestFixture]
+    public class SampleTest
+    {
+        [Test] // testApp is our Test Case
+        public void TestApp()
+        {
+            Console.WriteLine("Testing app");
+        }
+
+        [Test]
+        public void TestLogin()
+        {
+            Console.WriteLine("Login Test");
+        }
+    }
+}
---
+++ Test Project Demo/Yandex Test.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_Project_Demo
+{
+    [TestFixture]
+    public class YandexTest
+    {
+        [Test]
+        public void TestRecieveMail()
+        {
+            // Selenium code.
+            Console.WriteLine("Testing receiving emails");
+        }
+
+        [Test]
+        public void TestSendMail()
+        {
+            // Selenium code.
+            Console.WriteLine("Testing sending emails");
+        }
+
+        [SetUp]
+        public void OpenBrowser()
+        {
+            Console.WriteLine("Opening browser");
+        }
+
+        [TearDown]
+        public void CloseBrowser()
+        {
+            Console.WriteLine("Closing browser");
+        }
+    }
+}
